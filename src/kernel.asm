ee17f690ada7fb7c96c4dd470e521551bc9b2d72
Thalles Rodrigues <thallesr@gmail.com>
2023-11-02 21:56:53 +0200

    name fixes and printing letter on kernel.c

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,6 +1,6 @@
 [BITS 32]
 global _start ; exporting the symbol
-extern kernel_start
+extern kernel_main
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
 
--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,9 @@
 #include "kernel.h"
 
-void kernel_start(){
+void kernel_main()
+{
+    char* video_mem = (char *) (0xB8000);
+    video_mem[0] = 'A';
+    video_mem[1] = 1;
     
 }
