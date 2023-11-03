71339cb7324cfcddd9385ac4751f9edbd4a16067
Thalles Rodrigues <thallesr@gmail.com>
2023-11-03 14:27:44 +0200

    initializing video memory

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,27 @@
 #include "kernel.h"
 
+
 void kernel_main()
 {
-    char* video_mem = (char *) (0xB8000);
-    video_mem[0] = 'A';
-    video_mem[1] = 1;
+    terminal_initialize();
+
+}
+
+uint16_t terminal_make_char(char character,char colour){
+    return (colour<< 8) | character;
+    //notice the "reverse" order because of endiness;
+}
+
+
+void terminal_initialize(){
+    uint16_t * video_men = (uint16_t *) INITIAL_VIDEO_MEMORY;
+    //notice that making it a vector of a 16 bits makes things easier when addressing
+    for (int y  = 0; y < VGA_HEIGHT; y++)
+    {
+        for(int x = 0; x < VGA_WIDTH; x++){
+            video_men[y * VGA_WIDTH + x ] = terminal_make_char('A',15);
+        }
+    }
     
+
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,6 +1,16 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#include <stdint.h>
+
+#define INITIAL_VIDEO_MEMORY 0xB8000 
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 20
+
 void kernel_main();
+
+void terminal_initialize();
+
+uint16_t terminal_make_char(char character,char colour);
 
 #endif
