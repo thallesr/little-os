efb7fdf8bb5364f68d79dd23a2c1c22b163a07f0
Thalles Rodrigues <thallesr@gmail.com>
2024-01-05 16:55:55 +0200

    fixing error && instead of & on disk bit banging

--- src/disk/disk.c
+++ src/disk/disk.c
@@ -3,7 +3,7 @@ int disk_read_sector(int lba, int totalSectors, void * buffer){
 
     outb(0x1F6,(lba >> 24) | 0xE0);
     outb(0x1F2, totalSectors);
-    outb(0x1F3, (unsigned char)(lba && 0xff));
+    outb(0x1F3, (unsigned char)(lba & 0xff));
     outb(0x1F4, (unsigned char)(lba >> 8));
     outb(0x1F5, (unsigned char)(lba >> 16));
     outb(0x1F7, 0x20);
